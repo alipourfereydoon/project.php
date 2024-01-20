44c8c37c1a665b9ae7f0ff22b31ac95b6b88bea8
unknown <ali.pourferydoon1983@gmail.com>
2024-01-20 21:30:17 +0330

    this is the first change : project.php

---
+++ project.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- the first practice about php
+author is ali pourfereydoon
+
+-->
+<?php 
+echo "<b>the first project:  </b>"."Create a simple PHP program which displays your name, address, 
+and paragraph of information about you using the echo instruction.";
+echo"<br>";
+echo"--------------------------------------------------------------";
+echo "<br>";
+$name = "ali";
+$familyname="pourfereydoon";
+
+echo  " <b>my name is :</b>" .$name ;
+echo "<br>";
+echo  "<b>my family name is </b> :" . $familyname ;
+echo "<br>";
+
+echo  "<b>information about me </b>"."hi, this is my homepage, so I have to say something about myself. Sometimes
+it is hard to introduce yourself because you know yourself so well that you
+ do not know where to start with. 
+ I always wanted to be a great writer, like Victor Hugo  or like
+  Roman Roland . They have influenced millions of people through 
+  their books. I also wanted to be a great psychologist, like William James or Sigmund Freud, 
+  who could read people mind. Of course, I am nowhere close to these people, yet. I am just someone
+  who does some teaching, some research, and some writing. But my dream is still alive. ";
+
+?>
+</body>
+</html>
+
